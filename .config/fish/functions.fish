6781308ab57379565c71c8de93fb71c97203f06f
eecolella <eecolella@gmail.com>
2019-08-04 10:53:15 +0100

    misc

--- .config/fish/functions.fish
+++ .config/fish/functions.fish
@@ -1,13 +1,19 @@
 
-function fow -S -d '(F)fuzzy search a file and (O)open it in (W)webstorm'
-  find * -type f | fzf | read -l result; and webstorm "$result"
+function fow -S -d '(F)fuzzy search a name file and (O)open it in (W)webstorm'
+  find * -type f | fzf --preview 'bat --style=numbers --color=always {}' | read -l result; and webstorm "$result"
 end
 
+function fown -S -d '(F)fuzzy search a name file and (O)open it in (W)webstorm without (N)node_modules'
+  find * -type f | grep -v node_modules | fzf --preview 'bat --style=numbers --color=always {}' | read -l result; and webstorm "$result"
+end
 
 function fh -S -d '(F)fuzzy search a command in the (H)history'
   cat ~/.local/share/fish/fish_history | fzf | sed -e 's/- cmd://g' | pbcopy
 end
 
+function fcow -S -d '(F)fuzzy search a (C)content file and (O)open it in (W)webstorm'
+  rg --files-with-matches --no-messages $argv | fzf --preview 'bat --style=numbers --color=always {}' | read -l result; and webstorm "$result"
+end
 
 function reload -S -d 'Reload Fish config'
   source ~/.config/fish/config.fish
